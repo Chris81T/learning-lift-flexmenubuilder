8f10a882aa92f3a8259d779a5eeaebc9fc8b0ac0
Christian Thomas <christian@chr-thomas.de>
2012-12-13 20:07:22 +0100

    initial commit.

---
+++ src/main/scala/code/snippet/CustomMenu.scala
@@ -0,0 +1,17 @@
+package code.snippet
+
+import net.liftweb.sitemap.FlexMenuBuilder
+import net.liftweb.http.DispatchSnippet
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: christian
+ * Date: 12/13/12
+ * Time: 7:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object CustomMenu extends FlexMenuBuilder with DispatchSnippet {
+
+	def dispatch = null
+
+}
